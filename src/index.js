d41b1f6fb272856c58ee2696dfc481a03d1c4a19
gabrigode <gbarbosa7841@outlook.com>
2020-12-28 10:25:49 -0300

    O turns

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
 
-    render() {
-      return (
-        <button 
-          className="square" 
-          onClick={() => this.setState({value: 'X'})}
-        >
-          {this.state.value}
-        </button>
-      );
-    }
+  function Square(props){
+    return(
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    )
   }
   
   class Board extends React.Component {
@@ -26,7 +15,17 @@ class Square extends React.Component {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
+    }
+
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X': "O";
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      })
     }
 
     renderSquare(i) {
